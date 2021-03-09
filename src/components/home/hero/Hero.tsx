8122fe71c374f7df4d9c23b1bc58a995724274d0
ahmad jawabra <ahmad@terkwaz.com>
2021-03-09 17:04:46 +0200

    Finish Navbar component.

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,11 +1,12 @@
 import './hero.scss';
-import {Component} from "react";
+import { Component } from 'react';
+import { Navbar } from '../navbar/Navbar';
 
 export class Hero extends Component {
     render() {
         return (
             <div className="hero">
-
+                <Navbar/>
             </div>
         );
     }
--- src/components/home/navbar/Navbar.tsx
+++ src/components/home/navbar/Navbar.tsx
@@ -1,1 +1,33 @@
-import './navbar.sass';
+import './navbar.scss';
+import { Component, ReactNode } from 'react';
+
+interface INavbarState {
+    open: boolean;
+}
+
+export class Navbar extends Component<object, INavbarState> {
+    state = { open: false };
+
+    render(): ReactNode {
+        const { open } = this.state;
+
+        return (
+            <nav className={ `main-nav ${ open ? 'open' : '' }` }>
+                <div className="container">
+                    <div className="logo"><h2>NT<span>.</span></h2></div>
+
+                    <div className="btns-container">
+                        <button className="search-icon"><i className="fas fa-search"/></button>
+                        <button className="toggle-nav-btn" onClick={ () => this.setState({ open: !open }) }>menu</button>
+                    </div>
+
+                    <ul className="links-container">
+                        <li>
+                            <button>services</button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
